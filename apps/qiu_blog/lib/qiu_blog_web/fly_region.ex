1f711317fcafc63c29bfe5039e1e91a373814701
qhwa <qhwa@163.com>
2021-08-08 08:00:22 +0800

    debug

--- apps/qiu_blog/lib/qiu_blog_web/fly_region.ex
+++ apps/qiu_blog/lib/qiu_blog_web/fly_region.ex
@@ -2,24 +2,16 @@ defmodule QiuBlogWeb.FlyRegion do
   @behaviour Plug
 
   import Plug.Conn
-  require Logger
 
   @impl Plug
   def init(opts) do
-    Logger.info(inspect(opts))
     opts
   end
 
   @impl Plug
   def call(conn, opts) do
     conn
-    |> maybe_put_resp_header("x-fly-region", opts[:region])
-    |> maybe_put_resp_header("x-fly-alloc-id", opts[:alloc_id])
+    |> put_resp_header("x-fly-region", opts[:region] || "UNKNOWN")
+    |> put_resp_header("x-fly-alloc-id", opts[:alloc_id] || "UNKNOWN")
   end
-
-  defp maybe_put_resp_header(conn, name, value) when is_binary(value),
-    do: put_resp_header(conn, name, value)
-
-  defp maybe_put_resp_header(conn, _, _),
-    do: conn
 end
